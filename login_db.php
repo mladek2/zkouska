f345f84f85255094b1f817f21bd72c89572cf7e8
mladek2 <mladek2@users.noreply.github.com>
2015-06-10 09:07:26 +0200

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$link = mysql_connect('localhost', 'Jirka', '9302163332');
+$link = mysql_connect('localhost', 'Jirka', 'heslo_veslo');
   mysql_set_charset("utf8",$link);
 if (!$link||!mysql_select_db("Jirka")) {
     die('Could not connect: ' . mysql_error());
